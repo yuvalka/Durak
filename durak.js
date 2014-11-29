2ec4629a2f65c5d0eacfbc0c80a19964d9c42d6d
Yuval K <yuval.ka@gmail.com>
2014-11-29 18:58:33 +0200

    validation on attcking cards

--- durak.js
+++ durak.js
@@ -189,7 +189,11 @@ function Game() {
      */
     this.attack = function (cardsAttack) {
 
-        if (this.validateAttackCards(cardsAttack)) {
+        console.log('cards attacked');
+        console.log(cardsAttack);
+        $validate = this.validateAttackCards(cardsAttack);
+
+        if ($validate === true) {
 
             var game = this;
             var playerAttack = this.attacker;
@@ -199,7 +203,7 @@ function Game() {
                 var aCard = null;
                 var i = 0;
 
-                playerAttack.cards.forEach(function (card) {
+                playerAttack.cards.forEach(function (card) { // extra validation that attacking cards belong to attacker
                     if (card.number == cardAttack.number && card.shape == cardAttack.shape) {
                         aCard = playerAttack.cards.splice(i, 1)[0];
                         return;
@@ -212,7 +216,7 @@ function Game() {
                 }
             });
         }
-        else throw 'cannot attack with these cards';
+        else throw $validate;
     }
 
     /**
@@ -243,43 +247,70 @@ function Game() {
      * validate attack cards: same number and defender has at least the number of attacking cards
      * if board not empty, attacker cannot attack with cards that weren't played
      * @param cardsAttack
-     * @returns {boolean}
+     * @returns {boolean|string}
      */
     this.validateAttackCards = function(cardsAttack) {
 
         var game = this;
-        if (cardsAttack.length > this.getMaxCardsToAttack())
-            return false;
+        var error = null;
 
+        // check that all attacking cards belong to attacker
+        cardsAttack.forEach(function (cardAttack) {
+           var found = false;
+           game.attacker.cards.forEach(function(cardAttacker){
+               if (cardAttack.number == cardAttacker.number && cardAttack.shape == cardAttacker.shape) {
+                   found = true;
+               }
+           });
+           if (!found) {
+               error = 'one or more cards not found for attacker!';
+           }
+        });
+
+        // check that attacking cards don't exceed limit
+        if (!error && cardsAttack.length > this.getMaxCardsToAttack()) {
+            error = "attacking cards exceeded limit " + this.getMaxCardsToAttack();
+        }
+
+        if (error) return error; //TODO think of validator class..
+
+        // 1 cards it's ok
         if (cardsAttack.length == 1)
             return true;
 
+
         // no attack cards in board
         if (game.board.cardsAttack.length == 0) {
-            var isEqual = true;
-            var j = 1;
-            for (i = 0; i <= cardsAttack.length; i++) {
-                if (cardsAttack[j] != 'undefined' && cardsAttack[i].number != cardsAttack[j]) {
-                    isEqual = false;
-                    break;
-                }
-                j++;
-            }
-            return isEqual;
-        }else {
+            var attackNumber = null;
+            var attackShape  = null;
+            cardsAttack.forEach(function(cardAttack){
+                if (!attackNumber && !attackShape) {
+                    attackNumber = cardAttack.number;
+                    attackShape = cardAttack.shape;
+                } else {
+                    if (cardAttack.number != attackNumber) {
+                        error = 'attacking cards not equal in number';
+                    }
 
-           var isExistsOnBoard = true;
+                    if (cardAttack.shape == attackShape) {
+                        error = 'attacking cards has same shape';
+                    }
+                }
+            });
+
+        }else { // if we're here it means it's re-attacking
+
            var allCardsPlayed = game.board.cardsAttack.concat(game.board.cardsDefense);
            cardsAttack.forEach(function(cardAttack){
                allCardsPlayed.forEach(function(cardPlayed){
                   if (cardPlayed.number != cardAttack.number) {
-                      isExistsOnBoard = false;
+                      error = "card doesn't exist in board";
                   }
                });
            });
-
-           return isExistsOnBoard;
         }
+
+        return error ? error : true;
     }
 }
 
